e92d5eb4e93943a9c58dbb77249051ce20ab0034
Elango <117576351+En4128@users.noreply.github.com>
2025-02-10 18:49:29 +0530

    Update demo.java

--- demo.java
+++ demo.java
@@ -2,8 +2,6 @@ public class demo {
     public static void main(String[] args) {
         // Print a message to the console as output
         System.out.println("Hello all welcome to Jenkins");
-        System.out.println("Hello all welcome to Docker");
-        
-        
+        System.out.println("Hello all welcome to Docker");     
     } 
 }
